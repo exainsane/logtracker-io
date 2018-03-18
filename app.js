f11736da15bc450f78806ced7e53db7fcd5d8976
exain <rnugraha305@gmail.com>
2018-03-18 11:10:22 +0700

    Init express

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ socket.on('connection',(socket)=>{
     let handler = new SocketHandler(socket)
 })
 
-app.listen(3000,()=>{
+app.listen(process.env.PORT || 3000,()=>{
     console.log("Socket Server Started")
 })
 
