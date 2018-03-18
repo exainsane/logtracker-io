c3687bd7c3e6a53f756107f597b7255fc81830a5
exain <rnugraha305@gmail.com>
2018-03-18 10:45:44 +0700

    git commit -m "Add cors"

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 var app = require('express')
 var http = require("http").Server(app)
 var socket = require("socket.io")(http)
+var cors = require("cors")
 import SocketHandler from './handler/SocketHandler'
+
+app.use(cors())
+
 socket.on('connection',(socket)=>{
     let handler = new SocketHandler(socket)
 })
 
-http.listen(5000,()=>{
+http.listen(3000,()=>{
     console.log("Socket Server Started : " + http.address().port)
 })
 
